72609c2aefae7009d77d75846de2d8ec0ba8723c
Vanessa <vanessal2001@gmail.com>
2023-12-04 18:06:56 -0500

    hehehehehe xd!

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors({
     credentials: true,
@@ -35,3 +34,5 @@ UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 app.listen(process.env.PORT || 4000);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);
