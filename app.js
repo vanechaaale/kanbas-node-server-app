af88e4398c0294f6b5833d44178e06484f811c26
Vanessa <vanessal2001@gmail.com>
2023-12-03 22:31:10 -0500

    lol....

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
