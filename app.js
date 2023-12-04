05aa93941997475fb5c8a46aca3bc31b11d601d2
vanechaaale <vanessal2001@gmail.com>
2023-12-04 15:17:48 -0500

    i prayge

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import session from "express-session";
 import express from 'express'
 import cors from "cors";
 import mongoose from "mongoose";
@@ -8,8 +9,28 @@ import ModuleRoutes from "./modules/routes.js";
 
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+
+}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
+app.get('/hello', (req, res) => {res.send('Life is good!')})
 UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
